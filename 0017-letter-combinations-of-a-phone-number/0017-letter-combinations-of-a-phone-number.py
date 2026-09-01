924e7a44947b1130749e6d613126014161cbec06
Sabit Al Juma <sabitaljuma@gmail.com>
2026-09-01 16:48:28 +0600

    Time: 0 ms (100%), Space: 12.4 MB (58.11%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        str_dict= {'2':'abc', '3':'def', '4':'ghi', '5':'jkl','6':'mno','7':'pqrs',
+                    '8':'tuv', '9':'wxyz'}
+
+        result=['']
+        total=''
+        for i in digits:
+            new_result=[]
+            for j in result:
+                for k in str_dict[i]:
+                    new_result.append(j+k)
+            result=new_result
+        return result
+       
